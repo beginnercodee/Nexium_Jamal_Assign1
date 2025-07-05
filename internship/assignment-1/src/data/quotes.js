b3147daeb1a4127f8703b61e2fbadf5ff05dbfb9
JamalNadeem <jamalnadeem2004@gmail.com>
2025-07-05 21:34:35 +0500

    feat: add a quotes array

--- internship/assignment-1/src/data/quotes.js
+++ internship/assignment-1/src/data/quotes.js
@@ -4,16 +4,25 @@ export const quotesData = {
   success: [
     "Success is not in what you have, but who you are.",
     "Don’t wait for opportunity. Create it.",
-    "Success usually comes to those who are too busy to be looking for it."
+    "Success usually comes to those who are too busy to be looking for it.",
   ],
   motivation: [
     "Push yourself, because no one else is going to do it for you.",
     "Great things never come from comfort zones.",
-    "Don’t stop when you’re tired. Stop when you’re done."
+    "Don’t stop when you’re tired. Stop when you’re done.",
   ],
   coding: [
     "First, solve the problem. Then, write the code.",
     "Talk is cheap. Show me the code.",
-    "Experience is the name everyone gives to their mistakes."
+    "Experience is the name everyone gives to their mistakes.",
+  ],
+  quotes: [
+    "Believe you can and you're halfway there.",
+    "Your limitation—it’s only your imagination.",
+    "Push yourself, because no one else is going to do it for you.",
+    "Great things never come from comfort zones.",
+    "Dream it. Wish it. Do it.",
+    "Success doesn’t just find you. You have to go out and get it.",
+    "The harder you work for something, the greater you’ll feel when you achieve it."
   ]
 };
